9f1939554632e3956d0c634de57e2d529ddc679c
scorsi <sylvain.corsini@gmail.com>
2018-07-17 18:08:12 +0200

    Initial commit

---
+++ lib/aurorex.ex
@@ -0,0 +1,18 @@
+defmodule Aurorex do
+  @moduledoc """
+  Documentation for Aurorex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aurorex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/aurorex_test.exs
@@ -0,0 +1,8 @@
+defmodule AurorexTest do
+  use ExUnit.Case
+  doctest Aurorex
+
+  test "greets the world" do
+    assert Aurorex.hello() == :world
+  end
+end
